8eafc51c6bc7ff55ea301b4decc41c8ded0dd375
Gil Liang <81290935+Gil-Liang@users.noreply.github.com>
2026-05-28 19:36:24 +0800

    Chapter 2

---
+++ Chapter2/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Chapter2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNewWindow_Click(object sender, RoutedEventArgs e)
+        {
+            NewWindow winNew = new NewWindow();
+            winNew.Owner = this;
+            winNew.Show();
+        }
+    }
+}
